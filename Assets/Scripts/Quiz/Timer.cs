32859576b3a484bf1245dc747ad53f93a338e33c
Gustavo Alexandre <gustavoalexandredg@gmail.com>
2025-12-15 00:08:47 -0300

    configurações iniciais do cronometro

---
+++ Assets/Scripts/Quiz/Timer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+            timerText.color = Color.red;
+        }
+        int seconds = Mathf.FloorToInt(remainingTime);
+        timerText.text = seconds.ToString("00");
+    }
+}
